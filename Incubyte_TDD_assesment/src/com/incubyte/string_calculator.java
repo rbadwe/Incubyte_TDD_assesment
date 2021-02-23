88585cabd55a6620da6c80692320cb01ee0daded
rbadwe <rohan.badwe@outlook.com>
2021-02-23 18:22:50 +0530

    3rd commit.enabled the Add method to handle an unknown amount of numbers

--- Incubyte_TDD_assesment/src/com/incubyte/string_calculator.java
+++ Incubyte_TDD_assesment/src/com/incubyte/string_calculator.java
@@ -4,30 +4,33 @@ public class string_calculator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-      System.out.println( add("5656,2125"));
+      System.out.println( add("5656,2125,34343,3,9,00,2"));
        
 	}
 	
 	// The method can take up to two numbers, separated by commas, and will return their sum. 
 	public static int add(String numbers)
 
-	{
-		// assuming there are only 2 numbers 
-		
-
-		if(numbers=="1") return 1;
+	{		
+        // empty string
 		if(numbers =="") return 0;
+		
 		
 		if(numbers.contains(","))
 		{
-			String[] parts = numbers.split(",", 2);
-			String n1 = parts[0];
-			String n2 = parts[1];
+			// assuming there are only 2 numbers 
+			String[] parts = numbers.split(",");
+			int sum=0;
+			for(int i=0;i<parts.length;i++)
+			{
+				 sum += Integer.parseInt(parts[i]);
+			}
 			
-			int sum = Integer.parseInt(n1) + Integer.parseInt(n2);
 			return sum;
 		}
 		
-		return -1;// error 
+		
+		// only one digit 
+		return Integer.parseInt(numbers);
 	}
 }
