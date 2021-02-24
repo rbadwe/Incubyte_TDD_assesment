f50247bbb0b8e1c1800e866812347aab90decbf9
Rohan Badwe <39910043+rbadwe@users.noreply.github.com>
2021-02-24 17:49:27 +0530

    final commit
    added support for multiple delimiters of length 1 and above . Included all 9 test cases .

--- Incubyte_TDD_assesment/src/com/incubyte/string_calculator.java
+++ Incubyte_TDD_assesment/src/com/incubyte/string_calculator.java
@@ -1,98 +1,108 @@
 package com.incubyte;
+
 import java.util.*;
 
-
-// exception for negative numbers
-class MyException extends Exception
-{
- public MyException(ArrayList<Integer> str)
- {
-  System.out.println("negatives not allowed"+str);
- }
+// just a comment 
+// exception for negative number
+class MyException extends Exception {
+	public MyException(ArrayList<Integer> str) {
+		System.out.println("negatives not allowed" + str);
+	}
 }
+// 
+public class string_calculator {
 
-public class string_calculator  {
+	public static void main(String[] args) throws MyException {
 
-	public static void main(String[] args) throws MyException 
-	{
-		// TODO Auto-generated method stub
-      System.out.println( add("//[***]\n1***2***3"));
+		System.out.println(add("1,2")); // 1
+		System.out.println(add("1,6,7,8,4545"));// 2
+		System.out.println(add("1,6,7\n4545"));// 3
+		System.out.println(add("//;\n1;2")); // 4
+		System.out.println(add("//;\n1;2;-90;-4"));// 5
+		System.out.println(add("//;\n1;2;10000;999"));// 6
+		System.out.println(add("//[***]\n1***2***3"));// 7
+		System.out.println(add("//[*][%]\n1*2%3*7"));// 8
+		System.out.println(add("//[**][%%%]\n1**2%%%3**7"));//
 	}
 
-	
+	public static String delim(String num) {
+		String s = num;
+		s = s.substring(s.indexOf("[") + 1);
+		s = s.substring(0, s.indexOf("]"));
+		// System.out.println("delimiter is"+s);
+		String delimiter = s;
+
+		/// cut the initial defining part
+		// System.out.println("the ip string is "+
+		/// numbers.substring(numbers.indexOf("]") + 2));
+		num = num.substring(num.indexOf("]") + 2);
+
+		num = num.replaceAll(delimiter, ",");
+		return num;
+
+	}
+
 	public static int add(String numbers) throws MyException
 
-	{		
-        // empty string
-		if(numbers =="") return 0;
-		
-		String delimiter = ""+','; // the default delimiter
-	    // support different delimiters
+	{
+		// empty string
+		if (numbers == "")
+			return 0;
+
+		String delimiter = "" + ','; // the default delimiter
+		// support different delimiters
 		// case for string delimiters
-		
+
 		// getting rid of reserved characters ( in regex) replace with default delimiter
-		if(numbers.contains("*")) numbers = numbers.replace("*",delimiter);
-		if(numbers.startsWith("//["))
-		{
-			String s = numbers;
-			s = s.substring(s.indexOf("[") + 1);
-			s = s.substring(0, s.indexOf("]"));
-			//System.out.println("delimiter is"+s);
-			delimiter = s;
-			/// cut the initial defining part
-			//System.out.println("the ip string is "+ numbers.substring(numbers.indexOf("]") + 2));
-			numbers = numbers.substring(numbers.indexOf("]") + 2);
-			
-
+		if (numbers.contains("*"))
+			numbers = numbers.replace("*", delimiter);
+		if (numbers.startsWith("//[")) {
+			// more than 1 delimiter
+			while (!Character.isDigit(numbers.charAt(0))) {
+				numbers = delim(numbers);
+			}
 		}
-		else if(numbers.startsWith("//"))
-		{
-			delimiter = ""+numbers.charAt(2);
-			
-		//	System.out.println("delimiter is "+ delimiter);
-			// cut the initial part of numbers 
+
+		else if (numbers.startsWith("//")) {
+			delimiter = "" + numbers.charAt(2);
+
+			// System.out.println("delimiter is "+ delimiter);
+			// cut the initial part of numbers
 			numbers = numbers.substring(4);
-			
-		}
-		// allow new line 
-		numbers=numbers.replace("\n",delimiter);
-		
-		if(numbers.contains(delimiter))
-		{
-			// unknown number of numbers 
-			String[] parts = numbers.split(delimiter);
-			int sum=0 ;
-			ArrayList<Integer> al = new ArrayList<>(); 
-			for(int i=0;i<parts.length;i++)
-			{
-				
-				// negative exception
-				 int temp = Integer.parseInt(parts[i]);
-				 if(temp < 0) 
-					 al.add(Integer.valueOf(temp));
-				 // numbers greater than 1000 should be ignores 
-				 if(temp >1000)
-				 {
-					 continue;
-				 }
-				 sum += temp;
-			}
-			
-			try
-			{
-			 if(!al.isEmpty()) throw new MyException(al);
 
+		}
+		// allow new line
+		numbers = numbers.replace("\n", delimiter);
+
+		if (numbers.contains(delimiter)) {
+			// unknown number of numbers
+			String[] parts = numbers.split(delimiter);
+			int sum = 0;
+			ArrayList<Integer> al = new ArrayList<>();
+			for (int i = 0; i < parts.length; i++) {
+
+				// negative exception
+				int temp = Integer.parseInt(parts[i]);
+				if (temp < 0)
+					al.add(Integer.valueOf(temp));
+				// numbers greater than 1000 should be ignores
+				if (temp > 1000) {
+					continue;
+				}
+				sum += temp;
 			}
-			catch(MyException m)
-			{
+
+			try {
+				if (!al.isEmpty())
+					throw new MyException(al);
+
+			} catch (MyException m) {
 				System.out.println(m);
-						   
+
 			}
 			return sum;
 		}
-		
-		
-		// only one digit 
+
+		// only one digit
 		return Integer.parseInt(numbers);
 	}
-}
